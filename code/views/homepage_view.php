b69bd9377d379d04002a9fcc88c70c48b5f8cd88
Jordan Sleeper <findingDavincii@gmail.com>
2016-10-24 15:35:37 -0400

    reMMORPG is born

---
+++ code/views/homepage_view.php
@@ -0,0 +1,8 @@
+<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1.0"><title>recode:: MMORPG</title><link rel="stylesheet" href="styles/styles.css"></head><body>
+
+<h1>hello there</h1>
+
+<!-- scripts here -->
+<script src="vendor/jquery/jquery-3.1.1.min.js"></script>
+<script src="scripts/scripts.js"></script>
+</body></html>
